d328a1946c200baa7a5a7e7ecabe91cb7289754d
Humayra Khanum <humayra.cse01eng@gmail.com>
2021-03-13 19:13:07 +0600

    search music n lyrics

---
+++ app.js
@@ -0,0 +1,89 @@
+// CODED BY ODD HUMAYRA 
+
+// Arrow function for search button  starts here
+//#region 
+const searchSongs= () =>{
+    const searchText = document.getElementById('search-song-name').value ;
+    // console.log(searchText);
+    // Loading data from API 
+    const url =`https://api.lyrics.ovh/suggest /${searchText}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displaySongs(data.data))
+    .catch(error => displayError('Something went wrong! Please try again later!'))
+
+    // //async 
+    // const res = await fetch(url);
+    // const data = await res.json();
+    // displaySongs(data.data);
+
+}
+
+// Arrow function for search button ends here
+
+
+// Search button result starts here 
+const displaySongs = songs =>{
+    const songsContainer = document.getElementById('songs-container');
+    songsContainer.innerHTML = "";
+    songs.forEach( song =>{
+    //    const songContainer = document.createElement('song-container');
+    // console.log(song.title);
+    const songContainer = document.createElement('div');
+    songContainer.innerHTML = `
+    <div class="single-result row align-items-center my-3 p-3">
+                    <div class="col-md-9">
+                        <img src="${song.album.cover_small}" alt="Containing album's photo ">
+                        <h3 class="lyrics-name" id="lyric-name">${song.title}</h3>  
+                        <!-- Purple Noon -->
+                        <p class="author lead">Album by <span id="album-name">${song.artist.name}</span></p>  
+                        <!-- Album by <span>Washed Out</span> -->
+                        <audio controls>
+                        <source src=" ${song.preview}" type="audio/mpeg">
+                    </audio>
+                        
+                    </div>
+                    <div class="col-md-3 text-md-right text-center">
+                        <button onclick="getLyrics('${song.artist.name}' , '${song.title}')" class="btn btn-success">Get Lyrics</button>
+                    </div>
+                </div>
+    `
+    songsContainer.appendChild(songContainer);
+// document.getElementById('search-song-name').value = " ";
+        // const song = i;
+        // console.log(song);
+    //     console.log(i.title);
+    // console.log(i.artist.name);
+    // document.getElementById('lyric-name').innerText = (i.title); 
+    // document.getElementById('album-name').innerText = (i.artist.name); 
+    // document.getElementById('mp3').i
+    });
+}
+// Search Button results Ends here 
+
+const getLyrics =  ( artist,title) =>{
+// console.log(artist,title);
+const url = ` https://api.lyrics.ovh/v1/${artist}/${title}`;
+fetch(url)
+.then(response => response.json())
+.then(data => disPlayLyrics(data.lyrics));
+
+// const res = await fetch(url);
+// const  data = await res.json();
+// disPlayLyrics(data.lyrics); 
+}
+
+const disPlayLyrics = lyrics =>{
+    console.log(lyrics);
+    const wholeLyricDiv = document.getElementById('single-lyrics');
+    wholeLyricDiv.innerHTML = lyrics;
+    
+}
+// (songName , singerName)
+
+// Error function starts here
+const displayError = error =>{
+    const errorTag = document.getElementById('error-message').innerText = error;
+}
+
+//#endregion 
---
+++ forEach.js
@@ -0,0 +1,37 @@
+// let numbers = [1,2,3,4,5,6];
+
+// numbers.forEach(function (x){
+//     console.log(x);
+// })
+
+
+
+// let numbers = [1,2,3,4,5,6];
+
+//  numbers.forEach(function(){
+//     console.log(x);
+// })
+
+
+// let names = ["Humayra", "Nusrat", "Nazma", "Srity",  "safa", "Mariam" ,  "Razzak"];
+// let upperCase = [];
+
+// names.forEach(function(x){
+//       upperCase.push('Khan');
+//       console.log(upperCase);
+// });
+
+// forEach loop 
+// 
+
+// let ages = [25,26,30,55,21];
+
+// ages.forEach(function (parameter){
+//       console.log(parameter);
+// });
+
+let ages = [55,25,26,27,28,29,30];
+
+ages.forEach(function(parameter){
+      console.log(parameter+1);
+})
